06219060ccf4a164cd3280d915ccc2c316f58f5d
Blaine Buxton <btbuxton@gmail.com>
2017-11-07 17:47:10 -0500

    more fm madness

---
+++ bell_waves.rb
@@ -0,0 +1,17 @@
+# Bell waves
+use_synth :fm
+with_fx :flanger do
+  with_fx :echo, phase: 4 / 7.0 do
+    live_loop :sci_fi do
+      p = play (scale :D3, :egyptian).choose - [0, 12, -12].choose, pan: -0.5, divisor: 0.01, div_slide: rrand(0, 10), depth: rrand(0.001, 2), attack: 0.01, release: rrand(0, 5), amp: 0.5
+      control p, divisor: rrand(0.001, 10)
+      sleep [0.5, 1].choose
+    end
+    
+    live_loop :sci_fi_again do
+      p = play (scale :D2, :egyptian).choose - [0, 12, -12].choose, pan: 0.5, divisor: rrand(0.001, 5), depth: rrand(0.001, 2), attack: 0.01, release: rrand(0, 5), amp: 0.6
+      control p, divisor: 0.001
+      sleep [1.0 / 3, 1.5, 2.0 / 5].choose
+    end
+  end
+end
---
+++ future_residue.rb
@@ -0,0 +1,26 @@
+#future residue
+use_synth :fm
+
+root_note = :C2
+notes = scale(root_note, :blues_minor).collect { | each | midi_to_hz each }
+root = midi_to_hz root_note
+divs = notes.collect { | each | each / root }.shuffle
+print divs
+print divs.length
+#print [*0..12]
+use_bpm 60
+with_fx :echo, phase: 6/5.0, decay: 8 do
+  with_fx :slicer, phase: 2/3.0, pulse_width: 0.25 do
+    live_loop :drone do
+      p = play chord(:D3, :minor7).choose, depth: 0, divisor: 0, divisor_slide: 0.5, depth_slide: 0.5, pan_slide: 0.5, attack: 0, sustain: 16, release: 0
+      
+      16.times do
+        sleep 0.5
+        control p, depth: 4, divisor: divs.tick, pan: -0.5
+        sleep 0.5
+        control p, depth: 0, divisor: divs.tick, pan: 0.5
+      end
+    end
+  end
+end
+
